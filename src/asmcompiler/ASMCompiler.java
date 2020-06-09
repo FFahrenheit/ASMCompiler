38a1aeb7d690e927b9ba40e3c21ba2753db6b279
Iván L. Murillo <diskman199@gmail.com>
2020-06-09 12:09:31 -0500

    Initial commit

---
+++ src/asmcompiler/ASMCompiler.java
@@ -0,0 +1,10 @@
+package asmcompiler;
+
+public class ASMCompiler 
+{
+    public static void main(String[] args) 
+    {
+        MainPage compiler = new MainPage();
+        compiler.setVisible(true);
+    }    
+}
